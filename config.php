3c2665641cb0afc3b8c984003073ace19a0e959e
Papabilang21 <90594990+Papabilang21@users.noreply.github.com>
2021-09-13 14:34:30 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// JANGAN MENGUBAH APAPUN SELAIN YG DIDALAM TANDA (")
+// PERHATIKAN TANDA (") JANGAN SAMPAI TERHAPUS
+// AGAR SCRIPT TIDAK ERROR
+
+$user_agent = "Masukan user-agent Disini";
+
+$cookies = "Masukan Semua Cookie Disini";
+
